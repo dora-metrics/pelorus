beee9bf2a03f7f9e048bba36746a166ededf19a7
Andrew Block <andy.block@gmail.com>
2019-01-20 22:21:49 -0600

    First crack at automated setup

--- README.md
+++ README.md
@@ -1,3 +1,30 @@
 # Metrics Driven Transormation Quickstart
 
 Assets to rapidly demonstrate Metrics Driven Transformation (MDT) on the OpenShift Container Platform
+
+## Prerequisites
+
+* Ansible 2.7+
+* OpenShift Environment
+* OpenShift Command Line Tool
+
+## Configuration
+
+A bash script is used in combination with Ansible playbooks (some leveraging the [openshift-applier](https://github.com/redhat-cop/openshift-applier)) to build and provision the environment. The script makes use of several required parameters that must be provided either as environment variables or as script arguments.
+
+The following table describes the various configuration options:
+
+| Environment Variable | Command line Argument | Description | Default |
+| -------------------- | --------------------- | ----------- | ------- |
+| `OCP_SUBDOMAIN`      | `-s` or `--subdomain` | OpenShift default subdomain (ie: apps.openshift.example.com) |  |
+| `GITHUB_TOKEN`       | `-g` or `--github-token` | GitHub client token |  |
+| `JENKINS_NAMESPACE`  | `-j` or `--jenkins-namespace` | Project to deploy Jenkins | `hygieia` |
+| `HYGIEIA_NAMESPACE`  | `-h` or `--namespace` | Project to deploy all other resources | `hygieia` |
+
+## Provision
+
+Execute the followig command to provision the environment:
+
+```
+$ ./provision.sh -s=<subdomain> -g=<github_token>
+```
---
+++ provision.sh
@@ -0,0 +1,96 @@
+#!/bin/bash
+set -e
+
+SCRIPT_BASE_DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+DEFAULT_JENKINS_NAMESPACE=hygieia
+DEFAULT_HYGIEIA_NAMESPACE=hygieia
+TEMP_DIRECTORY=temp
+HYGIEIA_TOKEN_FILE=hygieia_token.txt
+GITEA_REPO_URL_FILE=gitea_repo.txt
+
+# Process Input
+for i in "$@"
+do
+  case $i in
+    -s=*|--subdomain=*)
+      OCP_SUBDOMAIN="${i#*=}"
+      shift;;
+    -g=*|--github-token=*)
+      GITHUB_TOKEN="${i#*=}"
+      shift;;
+    -j=*|--jenkins-namespace-=*)
+      JENKINS_NAMESPACE="${i#*=}"
+      shift;;
+    -n=*|--namespace=*)
+      HYGIEIA_NAMESPACE="${i#*=}"
+      shift;;
+    * )
+      echo "Invalid Option"
+      exit 1
+      ;;
+  esac
+done
+
+
+if [ -z "${OCP_SUBDOMAIN}" ]; then
+  echo "Error: OpenShift domain must be provided using the `-s` flag!"
+  exit 1
+fi 
+
+if [ -z "${GITHUB_TOKEN}" ]; then
+  echo "Error: GitHub token must be provided using the `-g` flag!"
+  exit 1
+fi
+
+if [ -z "${JENKINS_NAMESPACE}" ]; then
+  JENKINS_NAMESPACE="${DEFAULT_JENKINS_NAMESPACE}"
+fi
+
+if [ -z "${HYGIEIA_NAMESPACE}" ]; then
+  HYGIEIA_NAMESPACE="${DEFAULT_HYGIEIA_NAMESPACE}"
+fi
+
+echo 
+echo "## Provisioning MDT ##"
+echo
+
+echo "Downoading Ansible Galaxy Dependencies..."
+echo
+
+ansible-galaxy install -r "${SCRIPT_BASE_DIR}/requirements.yml" -p dependencies
+
+echo
+echo "Downloading Dependencies..."
+echo
+
+ansible-playbook -i localhost, "${SCRIPT_BASE_DIR}/playbooks/prep.yml"
+
+echo
+echo "Deploying Hygeia..."
+echo
+
+ansible-playbook -i "${SCRIPT_BASE_DIR}/dependencies/containers-quickstarts/hygieia/.applier" "${SCRIPT_BASE_DIR}/playbooks/hygieia.yml" -e filter_tags=all -e k8s_namespace="${HYGIEIA_NAMESPACE}" -e jenkins_namespace="${JENKINS_NAMESPACE}" -e openshift_default_subdomain="${OCP_SUBDOMAIN}" -e github_personal_access_token="${GITHUB_TOKEN}"
+
+echo
+echo "Deploying Gitea Server..."
+echo
+
+ansible-playbook -i "${SCRIPT_BASE_DIR}/.applier/gitea" "${SCRIPT_BASE_DIR}/dependencies/openshift-applier/playbooks/openshift-cluster-seed.yml" -e k8s_namespace="${HYGIEIA_NAMESPACE}" -e openshift_default_subdomain="${OCP_SUBDOMAIN}"
+
+echo
+echo "Configuring Gitea Server..."
+echo
+
+ANSIBLE_JINJA2_NATIVE=true ansible-playbook -i localhost, "${SCRIPT_BASE_DIR}/playbooks/gitea.yml" -e k8s_namespace="${HYGIEIA_NAMESPACE}"
+
+echo
+echo "Deploying Jenkins..."
+echo
+
+ansible-playbook -i "${SCRIPT_BASE_DIR}/dependencies/containers-quickstarts/jenkins-masters/hygieia-plugin/.applier" "${SCRIPT_BASE_DIR}/dependencies/openshift-applier/playbooks/openshift-cluster-seed.yml" -e hygieia_token=$(cat "${SCRIPT_BASE_DIR}/${TEMP_DIRECTORY}/${HYGIEIA_TOKEN_FILE}") -e hygieia_url="http://hygieia.${HYGIEIA_NAMESPACE}.${OCP_SUBDOMAIN}"  -e namespace="${HYGIEIA_NAMESPACE}"
+
+echo
+echo "Running Post Deployment Steps..."
+echo
+
+ansible-playbook -i localhost, "${SCRIPT_BASE_DIR}/playbooks/post.yml" -e k8s_namespace="${HYGIEIA_NAMESPACE}"
