b19ea2546923f1aa1cbc414fbb015ab5860d1d72
Thomas Peters-Hall <ramius345@gmail.com>
2019-12-02 11:12:07 -0600

    Use grafana operator

---
+++ charts/deploy/charts/grafana-operator/README.md
@@ -0,0 +1,5 @@
+This is based on the deployment code for this project:
+
+https://github.com/integr8ly/grafana-operator
+
+The deployment yamls are directly from this project and are licensed under the Apache License 2.0 (see template-LICENSE)
--- runhelm.sh
+++ runhelm.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
 export PROMETHEUS_HTPASSWD_AUTH=$(oc get secret prometheus-k8s-htpasswd -n openshift-monitoring -o jsonpath='{.data.auth}')
-export GRAFANA_DATASOURCE_PASSWORD=$(oc get secret grafana-datasources -n openshift-monitoring -o jsonpath='{.data.prometheus\.yaml}' | base64 -d | jq .datasources[0].basicAuthPassword)
+export GRAFANA_DATASOURCE_PASSWORD=$(oc get secret grafana-datasources -n openshift-monitoring -o jsonpath='{.data.prometheus\.yaml}' | base64 -d | jq .datasources[0].basicAuthPassword | sed 's/"//g' )
 export PROMETHEUS_HTPASSWD_AUTH=$(oc get secret prometheus-k8s-htpasswd -n openshift-monitoring -o jsonpath='{.data.auth}')
+
 
 helm template \
     --namespace pelorus \
     pelorus \
     --set openshift_prometheus_htpasswd_auth=$PROMETHEUS_HTPASSWD_AUTH \
-    --set internal_prometheus_basic_auth_pass=$GRAFANA_DATASOURCE_PASSWORD \
-    ./charts/deploy/ | oc apply -f - -n pelorus
+    --set openshift_prometheus_basic_auth_pass=$GRAFANA_DATASOURCE_PASSWORD \
+    ./charts/deploy/  | oc apply -f - -n pelorus
