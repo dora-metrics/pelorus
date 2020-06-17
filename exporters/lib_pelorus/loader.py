752aa8ddf19a3c09b0f9e26467dee709c8593ee8
Eric Sauer <esauer@redhat.com>
2020-06-17 11:54:56 -0400

    Refactor main committime method to reduce complexity

--- exporters/lib_pelorus/loader.py
+++ exporters/lib_pelorus/loader.py
@@ -28,7 +28,7 @@ def load_kube_config():
 
 def convert_date_time_to_timestamp(date_time):
     # Confirm we have a proper float value
-    float(date_time)
+    str(date_time)
     timestamp = datetime.strptime(date_time, '%Y-%m-%dT%H:%M:%SZ')
     unixformattime = timestamp.replace(tzinfo=timezone.utc).timestamp()
     return unixformattime
