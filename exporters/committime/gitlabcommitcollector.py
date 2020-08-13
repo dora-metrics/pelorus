e38f9bdddd3f8cef8603075caf9a5e02c4fdd050
Kurt Koller <kkoller@kkoller.iad2.csb>
2020-08-13 14:31:33 -0400

    removing extra files

--- exporters/committime/gitlabcommitcollector.py
+++
@@ -1,8 +0,0 @@
-class GitLabCommitCollector(AbstractCommitCollector):
-
-    def __init__(self, username, token, namespaces, apps):
-        super().__init__(username, token, namespaces, apps)
-
-    def get_commit_time(self):
-        """Method called to collect data and send to Prometheus"""
-        print("GitLab")
