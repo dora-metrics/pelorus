3206f48518e029f4f5b58c93e2f2a4d7f08c499d
Karl Moos <themoosman@users.noreply.github.com>
2020-08-25 08:52:37 -0500

    Implement GitLab collector (#163)
    * Add GitLab collector #107
    
    * Remove sample driver values.
    
    * Update PR to include issue #134
    
    * Update requirements.txt to include python-gitlab
    
    * Add some additional log statements.
    
    * Add all parm to project list
    
    * Remove cache code, use GL project search
    
    * Comment out a couple of lines used for testing.
    
    * Add DateFormat to base exporter and all impl, fixes for date formats
    
    * Update buildconfig to use ubi8/python-38 image as the base.
    
    * Make change from #171 so this PR can be tested without modifications.
    
    * Add a default parm to convert_date_time_to_timestamp so it's backwords compatible.
    
    * Change calculation for change failure rate (#162)
    
    * Change calc for cfr
    
    * Update chart version
    
    Co-authored-by: Mike LaCourse <mlacours@unknown60f262ee1fce.attlocal.net>
    
    * Update chart version.
    
    * Update Pelorus chart version
    
    Co-authored-by: Eric Sauer <esauer@redhat.com>
    Co-authored-by: Mike LaCourse <mlacours@redhat.com>
    Co-authored-by: Mike LaCourse <mlacours@unknown60f262ee1fce.attlocal.net>

--- exporters/committime/collector_bitbucket.py
+++ exporters/committime/collector_bitbucket.py
@@ -4,8 +4,8 @@ from collector_base import AbstractCommitCollector
 class BitbucketCommitCollector(AbstractCommitCollector):
 
     def __init__(self, username, token, namespaces, apps):
-        super().__init__(username, token, namespaces, apps)
+        super().__init__(username, token, namespaces, apps, "BitBucket", "TODO-DateFormat")
 
-    def get_commit_time(self):
+    def get_commit_time(self, metric):
         """Method called to collect data and send to Prometheus"""
         print("BitBucket is not yet supported")
