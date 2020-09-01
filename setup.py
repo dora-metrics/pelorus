2434033ee48ea0a4a763b9fd352f1208528d1b52
Eric Sauer <esauer@redhat.com>
2020-09-01 12:07:34 -0400

    Refactor tests into their own directory

---
+++ setup.py
@@ -0,0 +1,3 @@
+from setuptools import setup, find_packages
+
+setup(name="pelorus", packages=find_packages())
--- tests/test_pelorus.py
+++ tests/test_pelorus.py
@@ -1,6 +1,10 @@
 import os
 import pytest
+<<<<<<< HEAD
 from exporters import pelorus
+=======
+import pelorus
+>>>>>>> 642a19b... Refactor tests into their own directory
 
 
 @pytest.mark.parametrize("start_time,end_time,format",
