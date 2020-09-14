6e22950cbe830ee3389ad30680dcbb8a41acf3e8
Mike LaCourse <mlacours@unknown60f262ee1fce.attlocal.net>
2020-09-14 12:45:54 -0400

    Fix review issues

--- exporters/deploytime/app.py
+++ exporters/deploytime/app.py
@@ -14,6 +14,8 @@ k8s_config = client.Configuration()
 k8s_client = client.api_client.ApiClient(configuration=k8s_config)
 dyn_client = DynamicClient(k8s_client)
 
+supported_replica_objects = ['ReplicaSet', 'ReplicationController']
+
 
 class DeployTimeCollector(object):
     def __init__(self, namespaces):
@@ -61,35 +63,34 @@ def generate_metrics(namespaces):
     if not namespaces:
         logging.info("No namespaces specified, watching all namespaces")
     else:
-        logging.info("Wwtching namespaces %s", namespaces)
+        logging.info("Watching namespaces %s", namespaces)
 
     pods = v1_pods.get(label_selector=pelorus.get_app_label(), field_selector='status.phase=Running').items
 
     replicas_dict = {}
     # Process ReplicationControllers for DeploymentConfigs
-    get_replicas('v1', 'ReplicationController', replicas_dict)
+    replicas_dict = get_replicas('v1', 'ReplicationController')
 
     # Process ReplicaSets from apps/v1 api version for Deployments
-    get_replicas('apps/v1', 'ReplicaSet', replicas_dict)
+    replicas_dict.update(get_replicas('apps/v1', 'ReplicaSet'))
 
     # Process ReplicaSets from extentions/v1beta1 api version for Deployments
-    get_replicas('extensions/v1beta1', 'ReplicaSet', replicas_dict)
+    replicas_dict.update(get_replicas('extensions/v1beta1', 'ReplicaSet'))
 
     for pod in pods:
         if not namespaces or (pod.metadata.namespace in namespaces):
-            logging.info("Getting Replicas for pod: %s", pod.metadata.name)
+            logging.debug("Getting Replicas for pod: %s in namespace: %s", pod.metadata.name, pod.metadata.namespace)
             ownerRefs = pod.metadata.ownerReferences
             namespace = pod.metadata.namespace
 
             for ownerRef in ownerRefs:
-                if ownerRef.kind in ['ReplicaSet', 'ReplicationController'] and not pod_replica_dict.get(ownerRef.name):
+                if ownerRef.kind in supported_replica_objects and not not pod_replica_dict.get(ownerRef.name+namespace):
 
-                    logging.info("Getting Replica ownerRef: %s, %s",  ownerRef.name, ownerRef.kind)
+                    logging.debug("Getting replica: %s, kind: %s, namespace: %s",
+                                  ownerRef.name, ownerRef.kind, namespace)
 
-                    # Process ReplicationControllers for DeploymentConfigs
-                    rc = replicas_dict[ownerRef.name]
-
-                    if rc:
+                    if replicas_dict.get(ownerRef.name+namespace):
+                        rc = replicas_dict.get(ownerRef.name+namespace)
                         pod_replica_dict[rc.metadata.name] = "DONE"
                         images = [image_sha(c.image) for c in pod.spec.containers]
 
@@ -108,16 +109,18 @@ def generate_metrics(namespaces):
     return metrics
 
 
-def get_replicas(apiVersion, objectName, replicas):
-    # Process ReplicaSets from apps/v1 api version for Deployments
+def get_replicas(apiVersion, objectName):
+    # Process Replicas for given Api Version and Object type (ReplicaSet or ReplicationController)
+    replicas = {}
     try:
         apiResource = dyn_client.resources.get(api_version=apiVersion, kind=objectName)
         replicationobjects = apiResource.get(label_selector=pelorus.get_app_label())
         for replica in replicationobjects.items:
-            replicas[replica.metadata.name] = replica
+            replicas[replica.metadata.name+replica.metadata.namespace] = replica
     except ResourceNotFoundError:
         logging.debug("API Object not found for version: %s object: %s", apiVersion, objectName)
         pass
+    return replicas
 
 
 if __name__ == "__main__":
