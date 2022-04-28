a9dc31fa5d5b65238e85dbf6ff5b35ae0664f26a
Kevin M Granger <git@kevinmgranger.me>
2022-04-28 16:07:05 -0400

    Warn if a replicator is missing the app label

--- exporters/deploytime/app.py
+++ exporters/deploytime/app.py
@@ -4,7 +4,7 @@ import logging
 import os
 import re
 import time
-from typing import Iterable, Optional
+from typing import Any, Iterable, Optional
 
 import attr
 from kubernetes import client
@@ -19,14 +19,15 @@ supported_replica_objects = {"ReplicaSet", "ReplicationController"}
 
 
 class DeployTimeCollector:
-    _namespaces: set[str]
+    _warned_missing_replicas: set[str]
+    """Replicas with missing app labels that we've already warned about"""
 
-    def __init__(
-        self, namespaces: Iterable[str], client: DynamicClient, prod_label: str
-    ):
-        self._namespaces = set(namespaces)
+    def __init__(self, namespaces: set[str], client: DynamicClient, prod_label: str):
+        self._namespaces = namespaces
         self.client = client
         self.prod_label = prod_label
+        self._warned_missing_replicas = set()
+        self.app_label = pelorus.get_app_label()
 
     def collect(self) -> Iterable[GaugeMetricFamily]:
         logging.info("collect: start")
@@ -93,6 +94,23 @@ class DeployTimeCollector:
             )
         return namespaces
 
+    def _get_rc_app_label_or_warn(self, rc) -> Optional[str]:
+        """
+        Get the application label from the given replicator,
+        or log a warning once if it's not present.
+        """
+        if name := rc.metadata.labels[self.app_label]:
+            return name
+
+        full_path = f"{rc.metadata.namespace}/{rc.metadata.name}"
+        if full_path not in self._warned_missing_replicas:
+            logging.warn(
+                f"{rc.kind} {full_path} is missing app label {self.app_label}, skipping."
+            )
+            self._warned_missing_replicas.add(full_path)
+
+        return None
+
     def generate_metrics(
         self,
         namespaces: set[str],
@@ -110,7 +128,7 @@ class DeployTimeCollector:
         # get all running Pods with the app label
         v1_pods = self.client.resources.get(api_version="v1", kind="Pod")
         pods = v1_pods.get(
-            label_selector=pelorus.get_app_label(),
+            label_selector=self.app_label,
             field_selector="status.phase=Running",
         ).items
 
@@ -120,59 +138,67 @@ class DeployTimeCollector:
             | get_replicas(self.client, "extensions/v1beta1", "ReplicaSet")
         )
 
-        for pod in pods:
-            namespace = pod.metadata.namespace
-            owner_refs = pod.metadata.ownerReferences
-            if namespace not in namespaces or not owner_refs:
-                continue
+        pods = (
+            pod
+            for pod in pods
+            if pod.metadata.namespace in namespaces and pod.metadata.ownerReferences
+        )
+        pods_and_owners = (
+            (pod, owner_ref)
+            for pod in pods
+            for owner_ref in pod.metadata.ownerReferences
+        )
 
-            logging.debug(
-                "Getting Replicas for pod: %s in namespace: %s",
-                pod.metadata.name,
-                pod.metadata.namespace,
-            )
+        for pod, owner_ref in pods_and_owners:
+            namespace = pod.metadata.namespace
 
             # Get deploytime from the owning controller of the pod.
             # We track all already-visited controllers to not duplicate metrics per-pod.
-            for ref in owner_refs:
-                full_path = f"{namespace}/{ref.name}"
+            full_path = f"{namespace}/{owner_ref.name}"
 
-                if ref.kind not in supported_replica_objects or already_seen(full_path):
-                    continue
+            if owner_ref.kind not in supported_replica_objects or already_seen(
+                full_path
+            ):
+                continue
 
-                logging.debug(
-                    "Getting replica: %s, kind: %s, namespace: %s",
-                    ref.name,
-                    ref.kind,
-                    namespace,
+            logging.debug(
+                "Examining replica %s of kind %s for pod %s in namespace: %s",
+                owner_ref.name,
+                owner_ref.kind,
+                pod.metadata.name,
+                namespace,
+            )
+
+            if not (rc := replicas_dict.get(full_path)):
+                continue
+
+            mark_as_seen(full_path)
+
+            if not (name := self._get_rc_app_label_or_warn(rc)):
+                continue
+
+            container_shas = (
+                image_sha(container.image) for container in pod.spec.containers
+            )
+            container_status_shas = (
+                image_sha(status.imageID) for status in pod.status.containerStatuses
+            )
+            images = {sha for sha in container_shas if sha} | {
+                sha for sha in container_status_shas if sha
+            }
+
+            # Since a commit will be built into a particular image and there could be multiple
+            # containers (images) per pod, we will push one metric per image/container in the
+            # pod template
+            for sha in images:
+                metric = DeployTimeMetric(
+                    name=name,
+                    namespace=namespace,
+                    labels=rc.metadata.labels,
+                    deploy_time=rc.metadata.creationTimestamp,
+                    image_sha=sha,
                 )
-
-                if not (rc := replicas_dict.get(full_path)):
-                    continue
-
-                mark_as_seen(full_path)
-                container_shas = (
-                    image_sha(container.image) for container in pod.spec.containers
-                )
-                container_status_shas = (
-                    image_sha(status.imageID) for status in pod.status.containerStatuses
-                )
-                images = {sha for sha in container_shas if sha} | {
-                    sha for sha in container_status_shas if sha
-                }
-
-                # Since a commit will be built into a particular image and there could be multiple
-                # containers (images) per pod, we will push one metric per image/container in the
-                # pod template
-                for sha in images:
-                    metric = DeployTimeMetric(
-                        name=rc.metadata.labels[pelorus.get_app_label()],
-                        namespace=namespace,
-                        labels=rc.metadata.labels,
-                        deploy_time=rc.metadata.creationTimestamp,
-                        image_sha=sha,
-                    )
-                    yield metric
+                yield metric
 
 
 @attr.frozen(kw_only=True)
@@ -215,7 +241,7 @@ def image_sha(image_url_or_id: str) -> Optional[str]:
 
 def get_replicas(
     dyn_client: DynamicClient, apiVersion: str, objectName: str
-) -> dict[str, object]:
+) -> dict[str, Any]:
     """Process Replicas for given Api Version and Object type (ReplicaSet or ReplicationController)"""
     try:
         apiResource = dyn_client.resources.get(api_version=apiVersion, kind=objectName)
