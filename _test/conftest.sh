5226b90f6a8518f16b733e83a8b6219039b446e6
Michal Pryc <mpryc@redhat.com>
2022-07-14 12:46:14 +0200

    Updates to the Charts that allows to install Thanos and NooBaa
    Pelorus that is configured with Thanos requires small chart
    updates.
    
    We replace recommended instructions to install long term storage
    for development purposes using NooBaa instead of MinIO.

--- _test/conftest.sh
+++ _test/conftest.sh
@@ -30,14 +30,3 @@ setup_file() {
   print_info "${status}" "${output}" "${cmd}" "${tmp}"
   [ "$status" -eq 0 ]
 }
-
-@test "storage/minio-scc.yaml" {
-  tmp=$(split_files "storage/minio-scc.yaml")
-
-  namespaces=$(get_rego_namespaces "ocp\.deprecated\.*")
-  cmd="conftest test ${tmp} --output tap ${namespaces}"
-  run ${cmd}
-
-  print_info "${status}" "${output}" "${cmd}" "${tmp}"
-  [ "$status" -eq 0 ]
-}
