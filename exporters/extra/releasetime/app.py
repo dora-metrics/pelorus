b7ff9e539bf27a7b03492b8daa9060c9468d8b5c
Kevin M Granger <kgranger@redhat.com>
2022-09-14 14:54:39 -0400

    Set up logging for release time exporter
    Signed-off-by: Kevin M Granger <kgranger@redhat.com>

--- exporters/extra/releasetime/app.py
+++ exporters/extra/releasetime/app.py
@@ -3,9 +3,11 @@ import time
 from prometheus_client import start_http_server
 from prometheus_client.core import REGISTRY
 
+import pelorus
 from extra.releasetime import collector_github
 
 if __name__ == "__main__":
+    pelorus.setup_logging()
     collector = collector_github.make_collector()
 
     REGISTRY.register(collector)
