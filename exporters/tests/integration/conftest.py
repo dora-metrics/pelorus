d94bdc2a7d149144afc5656d6abbfc72bc8ef1cd
Kevin M Granger <kgranger@redhat.com>
2022-10-07 13:50:58 -0400

    Add mockoon tests for the bitbucket collector
    Signed-off-by: Kevin M Granger <kgranger@redhat.com>

---
+++ exporters/tests/integration/conftest.py
@@ -0,0 +1,11 @@
+import pytest
+from kubernetes.client import ApiClient, Configuration
+from openshift.dynamic import DynamicClient
+
+
+@pytest.fixture
+def openshift_client():
+    kube_config = Configuration()
+    kube_config.host = "https://localhost:3000"
+    kube_config.verify_ssl = False
+    return DynamicClient(ApiClient(configuration=kube_config))
---
+++ exporters/tests/integration/test_bitbucket.py
@@ -0,0 +1,30 @@
+import pytest
+from openshift.dynamic import DynamicClient
+
+from committime import CommitMetric
+from committime.collector_bitbucket import BitbucketCommitCollector
+
+
+@pytest.fixture
+def bitbucket_collector(openshift_client: DynamicClient):
+    return BitbucketCommitCollector(
+        openshift_client,
+        username="",
+        token="",
+        namespaces=[],
+        apps=[],
+        tls_verify=False,
+    )
+
+
+@pytest.mark.mockoon
+def test_time_retrieval(bitbucket_collector: BitbucketCommitCollector):
+    metric = CommitMetric("fake_app")
+    metric.repo_url = "http://127.0.0.1:3001/kgranger-rh2/bitbucket-committime-test"
+    metric.commit_hash = "1766a75de2be5cf7fc6bf1fb8424b6a9100485e4"
+
+    EXPECTED_TIMESTAMP = 1665100072
+
+    bitbucket_collector.get_commit_time(metric)
+
+    assert metric.commit_timestamp == EXPECTED_TIMESTAMP
