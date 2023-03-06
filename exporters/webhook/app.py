30bd6de2e6bf6cca5c55228acecb40391f2787d2
Michal Pryc <mpryc@redhat.com>
2023-03-06 15:07:57 +0100

    Update webhook port to 8080 to match other exporters
    We use one server to serve both /pelorus/webhook and the
    /metrics for prometheus. They should run on the same
    port as other exporters.
    
    Signed-off-by: Michal Pryc <mpryc@redhat.com>

--- exporters/webhook/app.py
+++ exporters/webhook/app.py
@@ -229,4 +229,4 @@ if __name__ == "__main__":
     collector = load_and_log(WebhookCommitCollector)
     REGISTRY.register(collector)
 
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=8080)
