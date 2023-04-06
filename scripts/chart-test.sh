fc26ac20a8a90ac65b5bf64288429545594cbc48
Michal Pryc <mpryc@redhat.com>
2023-04-06 11:43:44 +0200

    Pelorus-Operator 0.0.6 with fresh patches and updated release scripts
    This change includes Pelorus-Operator 0.0.6 which refreshes patches,
    updated linting of the charts so they now must match the pelorus-operator
    charts.
    
    Also it consists of change where failing patches were allowing to proceed
    with pelorus-operator creation.
    
    To create release it's enough to type: make release
    
    The RC releases are still two step as the create_pelorus_operator must
    have explicit rc version to be passed for the operator rc version.
    
    Signed-off-by: Michal Pryc <mpryc@redhat.com>

--- scripts/chart-test.sh
+++ scripts/chart-test.sh
@@ -5,8 +5,9 @@
 GIT_REPO=dora-metrics/pelorus.git
 REMOTE=origin
 ORIGIN=$(git remote show origin)
+ORIGIN_RET=$?
 
-if [ $? = 0 ]; then
+if [ $ORIGIN_RET == 0 ]; then
     if ! echo "$ORIGIN" | grep "$GIT_REPO" &> /dev/null; then
         REMOTE=upstream
     fi
@@ -21,3 +22,39 @@ git fetch "$REMOTE" --unshallow &> /dev/null
 git remote update upstream --prune &> /dev/null
 
 ct lint --remote "$REMOTE" --config ct.yaml
+
+# Verify the versions of the charts are the same across main charts and
+# the charts from pelorus-operator
+
+# Get the number representing all different versions found in the 'Chart.yaml'
+# files within charts/ and pelorus-operator/helm-charts/ directories
+FILES_VERSIONS=$(find charts/ pelorus-operator/helm-charts/ -type f -name 'Chart.yaml' -exec grep -H '^version: ' {} \;)
+VERSIONS=$(echo "$FILES_VERSIONS" | cut -d ' ' -f 2 | sort | uniq)
+NO_VERSIONS=$(echo "$VERSIONS" | wc -l)
+if [[ "$NO_VERSIONS" -eq 1 ]]; then
+    echo "All chart versions are in-sync. Chart version: $VERSIONS"
+else
+  echo "ERROR: Found different versions in Chart.yaml files:"
+  echo "$FILES_VERSIONS"
+  exit 1
+fi
+
+# Ensure version of the Grafana and Prometheus operators is in sync
+# with pelorus-operator.
+
+GRAFANA_VER_HELM=$(grep grafana_subscription_version charts/operators/values.yaml | cut -d':' -f2 | tr -d ' ')
+PROMETHEUS_VER_HELM=$(grep prometheus_subscription_version charts/operators/values.yaml | cut -d':' -f2 | tr -d ' ')
+
+if ! grep \""$GRAFANA_VER_HELM"\" pelorus-operator/bundle/metadata/properties.yaml >/dev/null; then
+  echo "ERROR: Grafana version $GRAFANA_VER_HELM not found in the pelorus-operator/bundle/metadata/properties.yaml"
+  exit 1
+else
+  echo "OK: Grafana version $GRAFANA_VER_HELM in sync with the pelorus-operator/bundle/metadata/properties.yaml"
+fi
+
+if ! grep \""$PROMETHEUS_VER_HELM"\" pelorus-operator/bundle/metadata/properties.yaml >/dev/null; then
+  echo "ERROR: Prometheus version $PROMETHEUS_VER_HELM not found in the pelorus-operator/bundle/metadata/properties.yaml"
+  exit 1
+else
+  echo "OK: Prometheus version $PROMETHEUS_VER_HELM in sync with the pelorus-operator/bundle/metadata/properties.yaml"
+fi
