705c36ea1548b86f8139372530d8dede61f449fc
Mateus Oliveira <msouzaol@redhat.com>
2023-05-09 10:29:27 -0300

    fix: Custom action pydantic attribute name
    Signed-off-by: Mateus Oliveira <msouzaol@redhat.com>

--- .github/workflow_templates/comment-pr/main.py
+++ .github/workflow_templates/comment-pr/main.py
@@ -6,7 +6,7 @@ from pydantic import BaseSettings, SecretStr
 
 class Settings(BaseSettings):
     input_token: SecretStr
-    pr_number: int
+    input_pr_number: int
     input_message: str
 
 
@@ -17,7 +17,7 @@ if __name__ == "__main__":
         logging.info(f"Loaded settings: {settings.json()}")
         github_api = Github(settings.input_token.get_secret_value())
         repo = github_api.get_repo("dora-metrics/pelorus")
-        pull_request = repo.get_issue(number=settings.pr_number)
+        pull_request = repo.get_issue(number=settings.input_pr_number)
         pull_request.create_comment(settings.input_message)
     except Exception as error:
         logging.error(f"An error ocurred: {error}")
