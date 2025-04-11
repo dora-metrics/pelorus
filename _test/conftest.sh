ae2a438bd99f79ada6cd1143a6b911f89586b607
Eric Sauer <esauer@redhat.com>
2025-04-11 13:52:24 -0400

    fix directory references for charts dir that was moved (#1177)
    Signed-off-by: Eric Sauer <esauer@redhat.com>

--- _test/conftest.sh
+++ _test/conftest.sh
@@ -9,19 +9,8 @@ setup_file() {
   conftest_pull
 }
 
-@test "charts/operators" {
-  tmp=$(helm_template "charts/operators")
-
-  namespaces=$(get_rego_namespaces "ocp\.deprecated\.*")
-  cmd="conftest test ${tmp} --output tap ${namespaces}"
-  run ${cmd}
-
-  print_info "${status}" "${output}" "${cmd}" "${tmp}"
-  [ "$status" -eq 0 ]
-}
-
 @test "charts/pelorus" {
-  tmp=$(helm_template "charts/pelorus")
+  tmp=$(helm_template "pelorus-operator/helm-charts/pelorus")
 
   namespaces=$(get_rego_namespaces "ocp\.deprecated\.*")
   cmd="conftest test ${tmp} --output tap ${namespaces}"
